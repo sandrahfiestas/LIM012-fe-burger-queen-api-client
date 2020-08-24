df69bfffa0d2906b9ebfb7480503376a818487ff
sandrahfiestas <shfiestas@gmail.com>
2020-08-23 23:38:39 -0500

    comentando codigo predeterminado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SummaryComponent } from './summary/summary.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SummaryComponent
   ],
   imports: [
     BrowserModule
