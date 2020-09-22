6ec43093c8ca124f450d1f7fc26271162235e51f
sandrahfiestas <shfiestas@gmail.com>
2020-09-22 18:15:52 -0500

    eliminando comentarios adicionales

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SummaryComponent } from './summary/summary.component';
+import { ProductComponent } from './product/product.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    SummaryComponent
+    SummaryComponent,
+    ProductComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
