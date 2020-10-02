bdba3bf12eaa2af430c3c62a022913114717764a
sandrahfiestas <shfiestas@gmail.com>
2020-10-02 13:52:28 -0500

    mostando hora con date()

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -8,9 +8,24 @@ declare var $: any;
 })
 export class NavbarComponent implements OnInit {
 
+  // hora
+  private date = new Date();
+
+  public hour: any;
+  public minute: string;
+  public ampm: string;
+
+
   constructor() { }
   
   ngOnInit(): void {
+
+    // hora
+    setInterval(() => {
+      const date = new Date();
+      this.updateDate(date);
+    }, 1000);
+
 
     $(document).ready(function () {
     $(".menu .btn").on("click", function () {
@@ -18,6 +33,20 @@ export class NavbarComponent implements OnInit {
     });
   });
 
+  }
+
+  private updateDate(date: Date){
+    const hours = date.getHours();
+
+    this.ampm = hours >= 12 ? 'PM' : 'AM';
+
+    this.hour = hours % 12;
+    this.hour = this.hour ? this.hour : 12;
+
+    this.hour = this.hour < 10 ? '0' + this.hour : this.hour;
+ 
+    const minutes = date.getMinutes();
+    this.minute = minutes < 10 ? '0' + minutes : minutes.toString();
   }
  
 }
