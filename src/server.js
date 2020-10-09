0d033584dab0893b353b618346f54c5ef937e65e
sandrahfiestas <shfiestas@gmail.com>
2020-10-08 19:22:44 -0500

    actualizaciones para heroku

--- src/server.js
+++ src/server.js
@@ -3,9 +3,9 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist/ng-blog'));
+app.use(express.static(__dirname+'/dist/LIM012-fe-burger-queen-api-client'));
 app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/ng-blog/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/LIM012-fe-burger-queen-api-client/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
